df047d203ea9175dbdb28878f362501254b1f69f
Jakob Petracek <209836@fhwn.ac.at>
2025-05-21 17:50:29 +0200

    window_manager class finished, i hope

--- include/window_manager.h
+++ include/window_manager.h
@@ -2,10 +2,10 @@
 #define window_manager_h
 
 #include <unordered_map> 
-#include <stdexcept>
+#include <vector>
+#include <expected>
 #include <memory> 
 #include <utility>
-#include <vector>
 
 #include "global_ncurses.h"
 #include "windows.h"
@@ -29,78 +29,16 @@ public:
     
     auto create_window(window&& other) -> std::expected<int, error>;
 
-    error remove_window(int id) { 
-
-        // remove all window_operator pairs with the window
-        std::erase_if(window_operator_pairs_, [&id](const auto& pair) {
-            return id == pair.first;
-        }); 
-
-        // remove from map 
-        if (auto it = windows_.find(id); it != windows_.end())
-        {
-            windows_.erase( it );
-            return ok;
-        }
-        
-        return not_found;
-    }
+    error remove_window(int id); 
 
     template <DerivedOperator Op, typename... Args>
-    auto create_operator(Args&& ...args) -> std::expected<int, error> 
-    {
-        auto res_pair = operator_.emplace( id_generator_++, std::make_unique<window>( std::forward<Args>(args)... ));
-        if (!res_pair.second) 
-            return std::unexpected(creation_failed);
+    auto create_operator(Args&& ...args) -> std::expected<int, error>;
+   
+    error remove_operator(int id);
 
-        return id_generator_;
-    }
-    
-    error remove_operator(int id) { 
-        
-        // remove all window_operator pairs with the operator
-        std::erase_if(window_operator_pairs_, [&id](const auto& pair) {
-            return id == pair.second;
-        });
+    error pair_window_operator(int window_id, int operator_id);
 
-        // remove from map 
-        if (auto it = operators_.find(id); it != operators_.end())
-        {
-            operators_.erase( it );
-            return ok;
-        }
-        
-        return not_found;
-    } 
-
-    error pair_window_operator(int window_id, int operator_id) {
-        
-        auto win_it = windows_.find(window_id);
-        auto op_it  = operators_.find(operator_id); 
-
-        if (win_it == windows_.end() || op_it == operators_.end() ) 
-            return not_found;
-
-        // call startup method of operator here
-        op_it->second->startup( *win_it->second );
-
-        // register the pair
-        if (auto res_pair = window_operator_pairs_.emplace_back(window_id, operator_id); 
-            res_pair.second)
-            return ok;
-        
-        return creation_failed;
-    }
-
-    void render() { 
-
-        // call render on all windows
-        for (std::pair<int, int>& pair : window_operator_pairs_) 
-        { 
-            std::unique_ptr<operator_base>& op = operators_.at(pair.second); 
-            op->render( *windows_.at(pair.first) );
-        }
-    }
+    void render();
 
     ~window_manager() = default;
  
--- src/window_manager.cpp
+++ src/window_manager.cpp
@@ -4,98 +4,98 @@
 
 namespace ui { 
 
-using namespace window_manager;
+using error = window_manager::error;
 
-std::expected<int, error> 
-    window_manager::create_window(int height, int width, int startx, int starty) { 
+auto window_manager::create_window(int height, int width, int startx, int starty) -> std::expected<int, error> {
+
     auto res_pair = windows_.emplace( id_generator_++, std::make_unique<window>(height, width, starty, startx));
     if (!res_pair.second)
-        std::unexpected(insertion_failed);
+        std::unexpected(creation_failed);
 
     return id_generator_;
 }
 
-std::expected<int, window_manager::error>
-    create_window(window&& other) {
-        auto res_pair = windows_.emplace( id_generator_++, std::make_unique<window>(other));
-        if (!res_pair.second)
-            return std::unexpected(insertion_failed);
+auto window_manager::create_window(window&& other) -> std::expected<int, error> {
 
-        return id_generator_;
-    } 
+    auto res_pair = windows_.emplace( id_generator_++, std::make_unique<window>(other));
+    if (!res_pair.second)
+        return std::unexpected(creation_failed);
+
+    return id_generator_;
+} 
 
 error window_manager::remove_window(int id) { 
 
-        // remove all window_operator pairs with the window
-        std::erase_if(window_operator_pairs_, [&id](const auto& pair) {
-            return id == pair.first;
-        }); 
+    // remove all window_operator pairs with the window
+    std::erase_if(window_operator_pairs_, [&id](const auto& pair) {
+        return id == pair.first;
+    }); 
 
-        // remove from map 
-        if (auto it = windows_.find(id); it != windows_.end())
-        {
-            windows_.erase( it );
-            return ok;
-        }
-        
-        return not_found;
-    }
-
-    template <DerivedOperator Op, typename... Args>
-    auto create_operator(Args&& ...args) -> std::expected<int, error> 
+    // remove from map 
+    if (auto it = windows_.find(id); it != windows_.end())
     {
-        auto res_pair = operator_.emplace( id_generator_++, std::make_unique<window>( std::forward<Args>(args)... ));
-        if (!res_pair.second) 
-            return std::unexpected(creation_failed);
-
-        return id_generator_;
+        windows_.erase( it );
+        return ok;
     }
+        
+    return not_found;
+}
+
+template <DerivedOperator Op, typename... Args>
+auto window_manager::create_operator(Args&& ...args) -> std::expected<int, error> {
+
+    auto res_pair = operator_.emplace( id_generator_++, std::make_unique<window>( std::forward<Args>(args)... ));
+    if (!res_pair.second) 
+        return std::unexpected(creation_failed);
+
+    return id_generator_;
+}
     
-    error remove_operator(int id) { 
+error window_manager::remove_operator(int id) { 
         
-        // remove all window_operator pairs with the operator
-        std::erase_if(window_operator_pairs_, [&id](const auto& pair) {
-            return id == pair.second;
-        });
+    // remove all window_operator pairs with the operator
+    std::erase_if(window_operator_pairs_, [&id](const auto& pair) {
+        return id == pair.second;
+    });
 
-        // remove from map 
-        if (auto it = operators_.find(id); it != operators_.end())
-        {
-            operators_.erase( it );
-            return ok;
-        }
+    // remove from map 
+    if (auto it = operators_.find(id); it != operators_.end())
+    {
+        operators_.erase( it );
+        return ok;
+    }
         
+    return not_found;
+} 
+
+error window_manager::pair_window_operator(int window_id, int operator_id) {
+        
+    auto win_it = windows_.find(window_id);
+    auto op_it  = operators_.find(operator_id); 
+
+    if (win_it == windows_.end() || op_it == operators_.end() ) 
         return not_found;
-    } 
 
-    error pair_window_operator(int window_id, int operator_id) {
+    // call startup method of operator here
+    op_it->second->startup( *win_it->second );
+
+    // register the pair
+    if (auto res_pair = window_operator_pairs_.emplace_back(window_id, operator_id); 
+        res_pair.second)
+        return ok;
         
-        auto win_it = windows_.find(window_id);
-        auto op_it  = operators_.find(operator_id); 
+    return creation_failed;
+}
 
-        if (win_it == windows_.end() || op_it == operators_.end() ) 
-            return not_found;
+void window_manager::render() { 
 
-        // call startup method of operator here
-        op_it->second->startup( *win_it->second );
-
-        // register the pair
-        if (auto res_pair = window_operator_pairs_.emplace_back(window_id, operator_id); 
-            res_pair.second)
-            return ok;
-        
-        return creation_failed;
+    // call render on all windows
+    for (std::pair<int, int>& pair : window_operator_pairs_) 
+    { 
+        std::unique_ptr<operator_base>& op = operators_.at(pair.second); 
+        op->render( *windows_.at(pair.first) );
     }
-
-    void render() { 
-
-        // call render on all windows
-        for (std::pair<int, int>& pair : window_operator_pairs_) 
-        { 
-            std::unique_ptr<operator_base>& op = operators_.at(pair.second); 
-            op->render( *windows_.at(pair.first) );
-        }
-    }
+}
 
 } // ui namespace 
 
