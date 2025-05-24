c76bb360d0eb7f0588d71854f95107a341fc7c7f
Jakob Petracek <209836@fhwn.ac.at>
2025-05-24 16:21:37 +0200

    Began writing sketch for the interpreter class. The implementation for the hashtabel is missing though so ill do that next

---
+++ include/interpreter.h
@@ -0,0 +1,42 @@
+#ifndef interpreter_h
+#define interpreter_h 
+
+#include <iostream>
+#include <spdlog/logger.h>
+#include <memory>
+#include "grid_base.h"
+#include "stack.h"
+#include "coroutine.h" 
+
+class Interpreter {
+public:
+
+    Interpreter(
+        grid_base& grid, 
+        std::istream& istream, 
+        std::ostream& ostream, 
+        std::shared_ptr<spdlog::logger>& logger
+    ) 
+    :   istream_(istream), ostream_(ostream), logger_(logger), coro_(interpret())
+    {}
+
+    operator bool() { return !!coro_; }
+    bool done() { return coro_.done(); }
+
+    void resume() { coro_.resume(); } 
+    void operator()() { coro_(); }
+
+    ~Interpreter() = default;
+
+private:
+    Stack<long> stack_;
+    std::istream& istream_;
+    std::ostream& ostream_;
+    std::shared_ptr<spdlog::logger> logger_;
+    Coroutine coro_;
+
+    Coroutine interpret() { co_return; }
+};
+
+
+#endif
