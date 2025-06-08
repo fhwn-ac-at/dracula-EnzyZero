f7540e2fc3ba82a227947e04a0304ba2cd8d2112
Jakob Petracek <209836@fhwn.ac.at>
2025-06-08 21:31:28 +0200

    Okay having real trouble finding a library that works with libstdc++, so I'll just use getopt

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class DraculaInterpreter(ConanFile):
         self.requires("gtest/1.16.0")
         self.requires("spdlog/1.15.1") 
         self.requires("fmt/11.1.3") 
-        self.requires("cxxopts/3.3.1")
+        self.requires("argparse/3.2")
         #self.requires("ncurses/6.4")
 
     def build_requirements(self):
