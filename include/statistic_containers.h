6db05d1e9d5d3923a6e55da43f1b5730ffdcf705
Jakob Petracek <209836@fhwn.ac.at>
2025-06-09 19:51:46 +0200

    Made a container which will be returned by each thread

---
+++ include/statistic_containers.h
@@ -0,0 +1,43 @@
+#ifndef statistic_containers_h
+#define statistic_containers_h
+
+#include <vector>
+#include <unordered_map>
+#include <compare> 
+
+#include "common.h"
+#include "../settings.h"
+
+struct ThreadResult {  
+
+private:
+  // trying an estimate for reserving ShortestGame vector
+  static constexpr int estimate_rolls = (settings::board::cols * settings::board::rows) / settings::dice::faces; 
+
+public: 
+    struct ShortestGame {
+
+      unsigned rolls{};
+      std::vector<int> roll_sequence; 
+
+      void reset() { rolls = 0; roll_sequence.clear(); }
+
+      bool operator==(const ShortestGame& other) const                  { return rolls ==  other.rolls; }
+      std::strong_ordering operator<=>(const ShortestGame& other) const { return rolls <=> other.rolls; }
+    };
+
+  ThreadResult()
+  : avrg_rolls{}, snakes_ladders_hits{}
+  {
+    // map will use as many keys as there are snakes and ladders
+    snakes_ladders_hits.reserve(settings::board::snakes_and_ladders.size());
+    shortest_game.roll_sequence.reserve(estimate_rolls);
+  }
+  
+  double avrg_rolls;
+  std::unordered_map<cmn::board_int_t, unsigned> snakes_ladders_hits;
+  ShortestGame shortest_game;
+
+};
+
+#endif
