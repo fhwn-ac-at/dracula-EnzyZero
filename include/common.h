2835782cdfea5fc6da2e44c156229597801e12f0
Jakob Petracek <209836@fhwn.ac.at>
2025-06-10 11:18:02 +0200

    Moved snakes stuff and dice things out of common into their own headers

--- include/common.h
+++ include/common.h
@@ -1,7 +1,6 @@
 #ifndef common_h
 #define common_h 
 
-#include <array>
 #include <concepts>
 
 namespace cmn {    
@@ -11,15 +10,11 @@ namespace cmn {
   
   template <std::integral T> 
   struct vector {
-    T y;
-    T x;
-  };
+    T y{};
+    T x{}; 
 
-  template<std::integral T, std::size_t C, std::size_t R>
-  using snakes_and_ladders_list_t = std::array<std::pair< vector<T>, vector<T> >, (C * R) / 2 - 2>; 
- 
-  template<std::integral T, std::size_t F>
-  using dice_weights_list_t = std::array<T, F>; 
+    constexpr bool is_null() const { return y == 0 && x == 0; }
+  };
 
 } // cmn namespace 
 
