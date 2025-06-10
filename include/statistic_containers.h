8d117fa8418a31d96be29646fdb6608edb669ec1
Jakob Petracek <209836@fhwn.ac.at>
2025-06-10 17:30:06 +0200

    Split definition and declaration of ThreadResult constructor

--- include/statistic_containers.h
+++ include/statistic_containers.h
@@ -6,6 +6,7 @@
 #include <compare> 
 
 #include "common.h"
+#include "snakes_ladders_list.h"
 #include "../settings.h" 
 
 struct GameStats {
@@ -23,23 +24,31 @@ struct ThreadResult {
 
 private:
   // trying an estimate for reserving ShortestGame vector
-  static constexpr int estimate_rolls = (settings::board::cols * settings::board::rows) / settings::dice::faces; 
+  static constexpr int _estimate_rolls = (settings::board::cols * settings::board::rows) / settings::dice::faces;
 
 public: 
     
-
-  ThreadResult()
-  : avrg_rolls{}, snakes_ladders_hits{}
-  {
-    // map will use as many keys as there are snakes and ladders
-    snakes_ladders_hits.reserve(settings::board::snakes_and_ladders.size());
-    shortest_game.roll_sequence.reserve(estimate_rolls);
-  }
+  ThreadResult();
   
   double avrg_rolls;
   std::unordered_map<cmn::board_int_t, unsigned> snakes_ladders_hits;
   GameStats shortest_game;
 
 };
+
+
+inline ThreadResult::ThreadResult()
+: avrg_rolls{}, snakes_ladders_hits{}
+{  
+  using namespace settings::board;
+
+  // get the keys ( the absolute origins ) and set their values to 0
+  constexpr auto view = snakes_and_ladders::list::to_absolute_positions<cmn::board_int_t, cols, rows>(settings::board::snakes_and_ladders) 
+    | std::ranges::views::transform([](const auto& val_pair) { return std::pair{val_pair.first, 0}; });
+
+  // assign to unordered_map and reserve some space to vector
+  snakes_ladders_hits.insert(view.begin(), view.end());
+  shortest_game.roll_sequence.reserve(_estimate_rolls);
+}
 
 #endif
