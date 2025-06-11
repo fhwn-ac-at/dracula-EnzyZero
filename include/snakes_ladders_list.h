1761eade94004de5770ae3ebe3e4552ec7f3366d
Jakob Petracek <209836@fhwn.ac.at>
2025-06-11 20:06:50 +0200

    Reworked that ugly snakes and ladders list

--- include/snakes_ladders_list.h
+++ include/snakes_ladders_list.h
@@ -1,81 +1,74 @@
 #ifndef snakes_ladders_list_h 
 #define snakes_ladders_list_h  
 
+#include <assert.h>
+#include <initializer_list>
 #include <concepts>
 #include <algorithm>
 #include <ranges>
 #include <array>
 #include <utility> 
 
-#include "common.h"
+#include "vec.h"
 
-namespace snakes_and_ladders {  
+namespace snakes_and_ladders {   
+  
+template <std::integral T, size_t C, size_t R>
+class list : public std::array<std::pair<vec<T>, vec<T>>, (C * R) / 2 - 2> { 
 
-  template <std::size_t C, std::size_t R>
-  constexpr std::size_t max = (C * R) / 2 - 2;
- 
-  namespace list { 
+  using _array = typename std::array<std::pair<vec<T>, vec<T>>, (C * R) / 2 - 2>;
+  using _vec_pair = typename _array::value_type;
 
-    // TODO add descriptions
-    template<std::integral T, std::size_t C, std::size_t R>
-    using type = std::array<std::pair<cmn::vector<T>, cmn::vector<T>>, max<C, R>>;    
+public:
 
-    template<std::integral T, std::size_t C, std::size_t R>
-    constexpr bool is_valid(type<T, C, R> list);
+  constexpr list(std::initializer_list<_vec_pair>& list);
 
-    template<std::integral T, std::size_t C, std::size_t R>
-    consteval auto to_absolute_positions(type<T, C, R> list) -> std::array<std::pair<T, T>, max<C, R>>;
+  constexpr operator bool() const { return _valid; }
+  constexpr bool is_valid() const { return operator bool(); }
+
+  constexpr auto to_abs_positions() -> std::array<std::pair<T, T>, this->size()>;
+
+private:
+  bool _check_vec(vec<T>& vec) { return vec.x <= 0 || vec.x > C || vec.y <= 0 || vec.y > R; } 
+
+  const _array::const_iterator _last_it;
+  bool _valid;
+}; 
 
 
-    
-    template<std::integral T, std::size_t C, std::size_t R>
-    constexpr bool is_valid(type<T, C, R> list) { 
 
-      using vec_pair = type<T, C, R>::value_type;
+template <std::integral T, size_t C, size_t R>
+constexpr list<T, C, R>::list(std::initializer_list<_vec_pair>& list) 
+  : _array( list ), 
+  _last_it( std::ranges::find_if(*this, [](const _vec_pair& pair) { return pair.first.is_null() && pair.second.is_null(); }) ), 
+  _valid(false)
+{
+  // zero out array if any of the vectors are out of bounds
+  if ( !std::ranges::all_of(this->begin(), _last_it, [this](const _vec_pair& pair) { return _check_vec(pair.first) && _check_vec(pair.second); } ))
+    std::ranges::fill(*this, {});
+  else
+    _valid = true;
+}
 
-      // helper lambda
-      auto check_vec = [](cmn::vector<T> vec) -> bool { return vec.x <= 0 || vec.x > C || vec.y <= 0 || vec.y > R; };
+template <std::integral T, size_t C, size_t R>
+constexpr auto list<T, C, R>::to_abs_positions() -> std::array<std::pair<T, T>, this->size()> 
+{  
+  assert(is_valid() && "this list is not valid");  
 
-      // get an iterator to the first empty snake or ladder
-      auto last_snake_or_ladder_it = std::ranges::find_if(list, [](const vec_pair& pair) { 
-        return pair.first.is_null() && pair.second.is_null(); 
-      });
-
-      // check if any of the coordinates are out of range, abort baord if yes
-      if (std::any_of(list.begin(), last_snake_or_ladder_it, [&check_vec](const vec_pair& pair) { return check_vec(pair.first) || check_vec(pair.second); })) 
-        return false;
-
-      return true;
-    }
-
-    template<std::integral T, std::size_t C, std::size_t R>
-    consteval auto to_absolute_positions(type<T, C, R> list) -> std::array<std::pair<T, T>, max<C, R>> {  
-
-      using abs_pair = std::pair<T, T>;
-      using vec_pair = typename type<T, C, R>::value_type; // look at snakes_and_ladders::list::type 
-
-      std::array<abs_pair, max<C, R>> ret_arr;   
- 
-      // 1. the code below first takes n elemenst which are not empty 
-      // 2. decrements the vector parts (needed for converting to absolute distance, which begins at 0 not 1)
-      // 3. sum the vectors coords together (first = first.x + first.y etc...)
-      auto view = list
-        | std::ranges::views::take_while([](const vec_pair& pair) {
-            return !pair.first.is_null() || !pair.second.is_null(); 
-        })
-        | std::ranges::views::transform([](const vec_pair& pair) {
-            return vec_pair( { .x = pair.first.x -1, .y = pair.first.y -1}, { .x = pair.second.x -1, .y = pair.second.y -1} );
-        })
-        | std::ranges::views::transform([](const vec_pair& pair) { 
-            return abs_pair(pair.first.y * R + pair.first.x, pair.second.y * R + pair.second.x);
-      }); 
-    
-      // copy and return
-      std::ranges::copy(view, ret_arr.begin());
-      return ret_arr;
-    }
-
-  } // list namespace 
+  // 1. the code below first takes n elemenst which are not empty 
+  // 2. decrements the vector parts (needed for converting to absolute distance, which begins at 0 not 1)
+  // 3. sum the vectors coords together (first = first.x + first.y etc...)
+  return *this
+  | std::ranges::views::take_while([](const _vec_pair& pair) {
+    return !pair.first.is_null() || !pair.second.is_null(); 
+  })
+  | std::ranges::views::transform([](const _vec_pair& pair) {
+    return vec_pair( { .x = pair.first.x -1, .y = pair.first.y -1}, { .x = pair.second.x -1, .y = pair.second.y -1} );
+  })
+  | std::ranges::views::transform([](const _vec_pair& pair) { 
+    return abs_pair(pair.first.y * R + pair.first.x, pair.second.y * R + pair.second.x);
+  }); 
+}
 
 } // snakes_and_ladders namespace 
 
