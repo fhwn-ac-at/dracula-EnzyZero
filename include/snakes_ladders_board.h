9a2ec43526d27f514c1f80099611199789f43a22
Jakob Petracek <209836@fhwn.ac.at>
2025-06-12 18:29:01 +0200

    Reworked snakes_and_ladders::board class, if I want a consteval construction then I'll hopefully be able to make a constexpr object

--- include/snakes_ladders_board.h
+++ include/snakes_ladders_board.h
@@ -1,88 +1,61 @@
 #ifndef board_init_h
 #define board_init_h
 
-#include <initializer_list> 
 #include <format>
 #include <stdexcept>
-#include <algorithm>
 
-#include "board.h"
-#include "common.h"
+#include "board_base.h"
 #include "snakes_ladders_list.h"
 
-
+namespace snakes_and_ladders {
+  
 template <std::integral T, size_t C = 10, size_t R = 10> 
-class SnakesLaddersBoard : public Board<T, C, R> {  
+class board : public board_base<T, C, R> {  
 public:
-
-  // using cstr's of Board
-  using Board<T, C, R>::Board; 
 
   /** 
    * @brief custom constructor whcih accepts and initializer list holding multiple fields
    * @exception Constructor may throw a std::logic_error if snakes or ladders are misplaced
   */
-  constexpr SnakesLaddersBoard(const std::initializer_list<T>& list);
-
-  /**
-   * @brief Use this consteval func to convert a snakes_and_ladders list into a board
-  */
-  static consteval auto init_board(const snakes_and_ladders::list::type<T, C, R>& list) -> SnakesLaddersBoard<T, C, R>;  
+  constexpr board(const snakes_and_ladders::list<T, C, R>& list);
 
 private:
-  using Board_t = Board<T, C, R>; 
+  using _base = board_base<T, C, R>; 
 };   
 
 
 template <std::integral T, size_t C, size_t R>
-constexpr SnakesLaddersBoard<T, C, R>::SnakesLaddersBoard(const std::initializer_list<T>& list) 
-: Board_t::_arr{}, Board_t::_init( false )
+constexpr board<T, C, R>::board(const snakes_and_ladders::list<T, C, R>& list)
+: _base::_arr{}, _base::_init( false )
 { 
-  // a second board which will be used to check for double destinations 
-  // (big memory usage, but O(1) access, and I dont wanna use unordered_map)
-  std::array<T, C * R> used_dest{};
+    assert(list && "List is invalid");
 
-  for(int i = 0; i < list.length(); i++) 
-  {      
-    const T dest = list.at(i); 
+    // a second board which will be used to check for double destinations 
+    // (big memory usage, but O(1) access, and I dont wanna use unordered_map)
+    std::array<T, C * R> used_dest{}; 
 
-    // check for false snakes or ladder placement
-    if (Board_t::_arr.at(i)    != 0 || 
-        Board_t::_arr.at(dest) != 0 ||
-        used_dest.at(dest)     != 0)
+    for(const auto [ abs_origin, abs_dest ] : list.to_abs_positions()) 
     { 
-      throw std::logic_error(std::format(
-        "STUPID::SNAKES::LADDER::PLACEMENT duplicate origin: {0} destination is an origin: {1} duplicate destination: {2}", 
-        Board_t::_arr.at(i)    == 0,
-        Board_t::_arr.at(dest) == 0,
-        used_dest.at(dest)     == 0)
-      );
-    }
+      // check for false snakes or ladder placement
+      if (_base::_arr.at(abs_origin) != 0 || 
+          _base::_arr.at(abs_dest)   != 0 ||
+          used_dest.at(abs_dest)     != 0)
+      { 
+        throw std::logic_error(std::format(
+          "STUPID::SNAKES::LADDER::PLACEMENT duplicate origin: {0} destination is an origin: {1} duplicate destination: {2}", 
+          _base::_arr.at(abs_origin) == 0,
+          _base::_arr.at(abs_dest)   == 0,
+          used_dest.at(abs_dest)     == 0)
+        ); 
+      } 
 
-    // save the index of the destination to this field
-    Board_t::_arr.at(i) = used_dest.at(dest) = dest;  
-  } 
+      // save the absolute index of the destination to an origin field 
+      _base::_arr[abs_origin] = used_dest[abs_dest] = abs_dest;  
+    } 
 
-  Board_t::_init = true;
-}
+  _base::_init = true;
+} 
 
-template <std::integral T, size_t C, size_t R>
-consteval auto SnakesLaddersBoard<T, C, R>::init_board(const snakes_and_ladders::list::type<T, C, R>& list) -> SnakesLaddersBoard<T, C, R> {
-
-  SnakesLaddersBoard<T, C, R> ret_board;  
-  ret_board.valid(false);  
-
-  if (!snakes_and_ladders::list::is_valid<T, C, R>(list))
-    return ret_board;
-  
-  for(const auto [ abs_origin, abs_dest ] : snakes_and_ladders::list::to_absolute_positions<T, C, R>(list)) 
-  {   
-    // save the absolute index of the destination to an origin field 
-    ret_board[abs_origin] = abs_dest;  
-  } 
-  
-  ret_board.valid(true);
-  return ret_board;
-}
+} // snakes and ladders namespace
 
 #endif
