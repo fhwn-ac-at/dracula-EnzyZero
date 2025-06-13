36611a832d23188e682188669953c42d77cf602d
Enzy <>
2025-06-13 19:47:24 +0200

    Added event-handler capabilities to game class to allow the statistics collector to register its methods to the appropriate events. I've done this to keep a clean interface and separate the collector and the game 'engine'

--- include/game.h
+++ include/game.h
@@ -7,11 +7,17 @@
 #include <dice.h>
 #include <variant>
 
+template <typename Cb>
+concept EventCallback =
+  std::invocable<Cb, unsigned> ||
+  std::invocable<Cb, size_t> ||
+  std::invocable<Cb>; 
+
 template <std::integral T, size_t C, size_t R, size_t F, bool goal_hit_exact = false> 
 class Game {
 public: 
 
-  constexpr Game(const board_base<T, C, R>& board, dice<T, F>&& d)
+  constexpr Game(const board_base<T, C, R>& board, Dice<T, F>&& d)
   : _board( board ), _board_max_pos( board.max_field_pos() ), _d( std::move(d) ), _won( false )
   { 
     assert(_board.valid() && "Board is invalid");
@@ -24,9 +30,7 @@ public:
 
   void reset() { _won = false; _pos = 0; } 
 
-  using event_arg_t = std::variant<size_t, unsigned, std::monostate>;
- 
-  static constexpr int events = 2; 
+  static constexpr int events = 3; 
   enum event : int {
     
     ROLL_EVENT,             // unsigned
@@ -34,35 +38,22 @@ public:
     WON_EVENT               // monostate
   }; 
 
-  void add_event_handler(const event ev, std::function<void(event_arg_t)>&& cb) { _event_cb.at(ev) = std::move(cb); }
+  template <EventCallback Cb>
+  void add_event_handler(const event ev, Cb&& cb) { _event_cb.at(ev) = _wrap_handler(std::move(cb)); }
 
 private:
   const board_base<T, C, R>& _board;
   const size_t _board_max_pos;
-  dice<T, F> _d;
+  Dice<T, F> _d;
 
   size_t _pos{};
   bool _won; 
   
-  std::array<std::function<void(event_arg_t)>, events> _event_cb{};
+  using _event_arg_t = std::variant<size_t, unsigned, std::monostate>;
+  std::array<std::function<void(_event_arg_t)>, events> _event_cb{};
 
-  template<typename CB>
-  std::function<void(event_arg_t)> _wrap_handler(CB&& cb) {  
-
-    // hack i got from chatGPT
-    return [f = std::forward<F>(cb)](event_arg_t arg) { 
-
-      std::visit([&](auto&& val) {
-        using Arg = std::decay_t<decltype(val)>; 
-
-        // Only call if the function is invocable with this type
-        if constexpr (std::is_invocable_v<CB, Arg>) {
-          f(val);
-        }
-      }, arg); // std::visit
-    };
-  } 
-
+  template<EventCallback Cb>
+  std::function<void(_event_arg_t)> _wrap_handler(Cb&& cb); 
 }; 
 
 
@@ -102,6 +93,24 @@ won:
   _won = true;
   if (_event_cb[WON_EVENT]) 
     _event_cb[WON_EVENT]();
-}
+} 
+
+template <std::integral T, size_t C, size_t R, size_t F, bool goal_hit_exact>
+template <EventCallback Cb>
+auto Game<T, C, R, F, goal_hit_exact>::_wrap_handler(Cb&& cb) -> std::function<void(typename Game<T, C, R, F, goal_hit_exact>::_event_arg_t)> {
+
+  // hack i got from chatGPT
+  return [f = std::forward<Cb>(cb)](_event_arg_t arg) { 
+
+    std::visit([&](auto&& val) {
+      using Arg = std::decay_t<decltype(val)>; 
+
+      // Only call if the function is invocable with this type
+      if constexpr (std::is_invocable_v<Cb, Arg>)
+        f(val);
+
+    }, arg); // std::visit
+  }; // lambda
+} 
 
 #endif
