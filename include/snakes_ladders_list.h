6c18e2730296b9362516ce33b4f2a720da399dd2
Jakob Petracek <209836@fhwn.ac.at>
2025-06-14 16:46:49 +0200

    wierdly this does not compile on constexpr anymore, so i improved the (I admit - smelly) class design and it no more inhertis from std::array but uses it as a member

--- include/snakes_ladders_list.h
+++ include/snakes_ladders_list.h
@@ -14,11 +14,11 @@
 namespace snakes_and_ladders {   
   
 template <std::integral T, size_t C, size_t R>
-class list : public std::array<std::pair<vec<T>, vec<T>>, (C * R) / 2 - 2> { 
+class list { 
 
   static constexpr size_t _Max = (C * R) / 2 - 2;
-  using _array = typename std::array<std::pair<vec<T>, vec<T>>, _Max>;
-  using _vec_pair = typename _array::value_type; 
+  std::array<std::pair<vec<T>, vec<T>>, _Max> _arr;
+  using _vec_pair = decltype(_arr)::value_type; 
 
 public:
 
@@ -27,28 +27,32 @@ public:
   constexpr operator bool() const { return _valid; }
   constexpr bool is_valid() const { return operator bool(); }
 
+  constexpr size_t size() const { return _arr.size(); }
+
   constexpr auto to_abs_positions() const -> std::array<std::pair<T, T>, _Max>;
 
 private:
-  bool _check_vec(const vec<T>& vec) { return vec.x <= 0 || vec.x > C || vec.y <= 0 || vec.y > R; } 
+  constexpr bool _check_vec(const vec<T>& vec) { return vec.x <= 0 || vec.x > C || vec.y <= 0 || vec.y > R; } 
 
-  const _array::const_iterator _last_it;
   bool _valid;
 }; 
 
 
-
 template <std::integral T, size_t C, size_t R>
 constexpr list<T, C, R>::list(std::initializer_list<_vec_pair> list) 
-  : _array{}, 
-    _last_it( std::ranges::find_if(*this, [](const _vec_pair& pair) { return pair.first.is_null() && pair.second.is_null(); }) ), 
+  : _arr{},
     _valid(false)
 { 
-  std::copy(list.begin(), list.end(), this->begin());
+  std::ranges::copy(list, _arr.begin());
+  
+  // find the last position with a snake or ladder
+  const auto _last_it = std::ranges::find_if(_arr, [](const _vec_pair& pair) { 
+    return pair.first.is_null() && pair.second.is_null(); 
+  }); 
 
   // zero out array if any of the vectors are out of bounds
-  if ( !std::ranges::all_of(this->begin(), _last_it, [this](const _vec_pair& pair) { return _check_vec(pair.first) && _check_vec(pair.second); } ))
-    std::ranges::fill(*this, _vec_pair{});
+  if ( !std::ranges::all_of(_arr.begin(), _last_it, [this](const _vec_pair& pair) { return _check_vec(pair.first) && _check_vec(pair.second); } ))
+    std::ranges::fill(_arr, _vec_pair{});
   else
     _valid = true; 
 }
@@ -61,7 +65,7 @@ constexpr auto list<T, C, R>::to_abs_positions() const -> std::array<std::pair<T
   // 1. the code below first takes n elemenst which are not empty 
   // 2. decrements the vector parts (needed for converting to absolute distance, which begins at 0 not 1)
   // 3. sum the vectors coords together (first = first.x + first.y etc...)
-  auto view = *this
+  auto view = _arr 
   | std::ranges::views::take_while([](const _vec_pair& pair) {
     return !pair.first.is_null() || !pair.second.is_null(); 
   })
